7b5de422c3c84e75c0fea69d176f5a4c97ffb2f0
muazinhsb <muazinh11@gmail.com>
2022-04-02 00:57:45 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "value")
+	os.Setenv("CHANNEL_ID", "value")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"sample.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name : %s, URL : %s\n", file.Name, file.URL)
+	}
+}
